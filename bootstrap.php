8a6198af89d42afe720dbdbe202c795639dbc016
Matheus E. Muller <hello@memuller.com>
2011-08-15 13:52:16 -0300

    Adds basic Silex structure.

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php 
 	define('APP_ROOT', getcwd());
+
+	require_once APP_ROOT . '/vendors/silex.phar' ;
 	require_once APP_ROOT . '/vendors/doctrine_mongo/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
 	require_once 'vendors/bootstrapers/doctrine.php';
 
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 	require_once 'bootstrap.php' ;
+
+	$app = new Silex\Application();
+
+	$app->get('/', function() use($app) {
+		return 'Hey.' ; 
+	}); 
+
+	$app->run(); 
+
 ?>

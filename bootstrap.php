712398615c7ef1fedd6dd00fbbfd48304c38e9a6
Matheus E. Muller <hello@memuller.com>
2011-08-15 13:10:18 -0300

    Working bootstrap for Doctrine and Models.

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,12 @@
 <?php 
+	define('APP_ROOT', getcwd());
+	require_once APP_ROOT . '/vendors/doctrine_mongo/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
+	require_once 'vendors/bootstrapers/doctrine.php';
 
+	use Doctrine\Common\ClassLoader ;
+
+	# loads all models.
+	$classLoader = new ClassLoader('Models', APP_ROOT . '/models') ;
+	$classLoader->register();
 
  ?>

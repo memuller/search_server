b8af3147a6fbec793494aa1ecbd0cd9f81ff7707
Matheus E. Muller <hello@memuller.com>
2011-10-17 17:22:11 -0200

    Basic field validations for Documents.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,12 @@
 class Document
   include Mongoid::Document
-  field :title, :type => String
-  field :uri, :type => String
+  field :title, type: String
+  field :uri, type: String
+  field :excerpt, type: String
+
+  index :title
+  index :uri, unique: true
+
+  validates_presence_of :title
+  validates :uri, presence: true, uniqueness: {case_sensitive: false, message: 'already indexed'}
 end

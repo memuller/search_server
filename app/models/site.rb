808b673a3db013b5e86854c6a454470b93a9c7c0
Matheus E. Muller <hello@memuller.com>
2011-11-01 19:12:05 -0200

    Adds slug field to site.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,16 +1,19 @@
 class Site
   include Mongoid::Document
-  field :name, :type => String
-  field :uri, :type => String
+  field :name, type: String
+  field :uri, type: String
+  field :slug, type: String
 
   has_many :documents, :dependent => :destroy
 
   index :name
-  index :uri, :unique => true
+  index :uri, unique: true
+  index :slug, unique: true
   index :document_ids
 
   validates_presence_of :name
   validates :uri, presence: true, uniqueness: { case_sensitive: false, message: 'already here' }
+  validates :slug, presence: true, uniqueness: { case_sensitive: false }
 
   def self.parse_and_create(url)
   	url = URI.parse(url)
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -5,15 +5,18 @@ describe Site do
 	describe 'fields and validations' do
 		it { should have_field(:uri).of_type(String) }
 		it { should have_field(:name).of_type(String) }
+		it { should have_field(:slug).of_type(String) }
 
 		it { should validate_presence_of(:uri) }
 		it { should validate_uniqueness_of(:uri).case_insensitive.with_message('already here') }
 		it { should validate_presence_of(:name) }
+		it { should validate_uniqueness_of(:slug).case_insensitive }
 	end
 	
 	describe 'indexes' do
 		it { Site.index_options[:name].should_not be nil  }
-		it { Site.index_options[:uri][:unique].should == true }
+		it { Site.index_options[:uri][:unique].should be true }
+		it { Site.index_options[:slug][:unique].should be true }
 	end
 
 	context "relationship with documents" do
@@ -21,20 +24,29 @@ describe Site do
 		it { Site.index_options[:document_ids].should_not be nil }
 	end
 
-	context "parsing an URL into sites" do
+	context "generating slug" do
+		describe ""
+	end
+
+
+	context "parsing an URL into a site" do
 		before :each do
 			@full_url = "http://blog.cancaonova.com/"
 			@url_with_subsite = "http://blog.cancaonova.com/revolucao"
 			@host_only = "blog.cancaonova.com"
-			Site.destroy_all
+			Site.delete_all
 		end 
-		describe "its parse_and_create class method" do
-			it "should exist, receiving an argument" do
-				Site.should respond_to :parse_and_create
-				->{Site.parse_and_create(@full_url)}.call.should_not raise_error ArgumentError
-			end
+		describe "parse_and_create class method" do
+			subject {Site}
+			it { should respond_to :parse_and_create }
+			it { ->{Site.parse_and_create(@full_url)}.should_not raise_error ArgumentError }
+			context "invalid url provided" do
+				let(:invalid_url){"not_an_uri"}
+				it{ ->{ Site.parse_and_create(invalid_url) }.should_not raise_error }
+				it{ ->{ Site.parse_and_create(invalid_url) }.call }
 
-			it "should a site object with the provided url" do
+			end
+			it "creates a site with proper URI" do
 				response = Site.parse_and_create(@full_url)
 				response.uri.should eq @host_only
 			end

aefcda5a23e5a22adc6d04f98b27dee98225f08d
Matheus E. Muller <hello@memuller.com>
2011-11-01 19:31:07 -0200

    Minor refactoring of site specs.

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe Site do
 
-	describe 'fields and validations' do
+	describe 'fields' do
 		it { should have_field(:uri).of_type(String) }
 		it { should have_field(:name).of_type(String) }
 		it { should have_field(:slug).of_type(String) }
-
+	end
+	describe 'validations' do
 		it { should validate_presence_of(:uri) }
 		it { should validate_uniqueness_of(:uri).case_insensitive.with_message('already here') }
 		it { should validate_presence_of(:name) }
@@ -14,9 +15,10 @@ describe Site do
 	end
 	
 	describe 'indexes' do
-		it { Site.index_options[:name].should_not be nil  }
-		it { Site.index_options[:uri][:unique].should be true }
-		it { Site.index_options[:slug][:unique].should be true }
+		subject { Site.index_options }
+		its ([:name]) { should_not be nil }
+		its ([:uri]) { should eq unique: true }
+		its ([:slug]) { should eq unique: true }
 	end
 
 	context "relationship with documents" do
@@ -25,7 +27,8 @@ describe Site do
 	end
 
 	context "generating slug" do
-		describe ""
+		describe "generate_slug class method" do
+		end
 	end
 
 

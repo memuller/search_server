5a5141d8f94c5d9edcfe4958cc48b439e2f6f07f
Matheus E. Muller <hello@memuller.com>
2012-01-05 16:08:03 -0200

    Tries to fix DRB:Unknown errors (in vain =( )

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,34 +7,34 @@ Spork.prefork do
   
   ENV["RAILS_ENV"] ||= 'test'
 
-  # does not load 
+  require 'rails/application'
   require 'rails/mongoid'
   Spork.trap_class_method(Rails::Mongoid, :load_models)
   
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  #require 'capybara/rspec'
 
   require Rails.root.join('spec/support/mongoid.rb')
-  #require Rails.root.join('spec/support/blueprints.rb')
 
   RSpec.configure do |config|
     config.mock_with :rspec
-
+    
+    config.include Mongoid::Matchers
+    
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+    
+    config.before :all do
+      Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+    end
   
   end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-  Fabrication.clear_definitions
-  RSpec.configure do |config|
-    config.before :all do
-      Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
-    end
-  end
+  #Fabrication.clear_definitions
+  
 end

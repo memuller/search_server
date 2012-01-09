0cd0278d899412bf0869fb1f30934bbfb9704a51
Matheus E. Muller <hello@memuller.com>
2012-01-09 14:37:54 -0200

    Adds view tests for the home page

--- spec/views/sites/index.html.haml_spec.rb
+++ spec/views/sites/index.html.haml_spec.rb
@@ -2,16 +2,40 @@ require 'spec_helper'
 
 describe "sites/index.html.haml" do
   before(:each) do
-    assign(:sites, [
+    @sites = [
       Fabricate(:site), Fabricate(:site)
-    ])
+    ]
+    assign(:sites, @sites )
+  end
+
+  describe "fields that will be show" do
+    it "will show URI" do
+      render
+      assert_select "th", count: 1, text: "Uri"
+      assert_select "td", count: 1, text: @sites.first.uri
+    end
+    it "will show title" do
+      render
+      assert_select "th", count: 1, text: "Name"
+      assert_select "td", count: 1, text: @sites.first.name
+    end
   end
 
   it "renders a list of sites" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr",  :count => 2 + 1
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr",  :count => 2 + 1  
+  end
+
+  it "shows a link to the site's documents" do
+    render
+    assert_select "a[href=#{site_documents_path(@sites.first.slug) }]", count: 1, text: 'Documents'  
+  end
+
+  describe "general links" do
     
+    it "shows a link to create new sites" do
+      render
+      assert_select "a[href=#{new_site_path()}]", count: 1, text: 'New Site'
+    end
   end
 end
